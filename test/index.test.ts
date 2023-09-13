9d74557a24d39c128d114569c0088b0f80d65b36
Junaid <86780488+jdevcs@users.noreply.github.com>
2023-09-13 16:19:48 +0200

    initial settings (#1)
    * initial settings
    
    * semver start
    
    * updated libs
    
    * plugin name and test updates
    
    * test updates
    
    * test updates

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,21 +1,27 @@
 import Web3, { core } from "web3";
-import { TemplatePlugin } from "../src";
+import { EIP4337Plugin } from "../src";
 
-describe("TemplatePlugin Tests", () => {
+describe("EIP4337Plugin Tests", () => {
   it("should register TokensPlugin plugin on Web3Context instance", () => {
     const web3Context = new core.Web3Context("http://127.0.0.1:8545");
-    web3Context.registerPlugin(new TemplatePlugin());
-    expect(web3Context.template).toBeDefined();
+    web3Context.registerPlugin(new EIP4337Plugin());
+    expect(web3Context.EIP4337).toBeDefined();
   });
 
-  describe("TemplatePlugin method tests", () => {
+  it("should register TokensPlugin plugin on Web3 instance", () => {
+    const web3 = new Web3("http://127.0.0.1:8545");
+    web3.registerPlugin(new EIP4337Plugin());
+    expect(web3.EIP4337).toBeDefined();
+  });
+
+  describe("EIP4337Plugin method tests", () => {
     let consoleSpy: jest.SpiedFunction<typeof global.console.log>;
 
     let web3Context: Web3;
 
     beforeAll(() => {
       web3Context = new Web3("http://127.0.0.1:8545");
-      web3Context.registerPlugin(new TemplatePlugin());
+      web3Context.registerPlugin(new EIP4337Plugin());
       consoleSpy = jest.spyOn(global.console, "log").mockImplementation();
     });
 
@@ -24,7 +30,7 @@ describe("TemplatePlugin Tests", () => {
     });
 
     it("should call TempltyPlugin test method with expected param", () => {
-      web3Context.template.test("test-param");
+      web3Context.EIP4337.test("test-param");
       expect(consoleSpy).toHaveBeenCalledWith("test-param");
     });
   });
