d450e2929e7193a52678c1488c2cd4bfee235c64
sanyu1225 <pan831225@gmail.com>
2023-11-26 00:38:08 +0800

    fix: use web3-validator

--- src/validator/index.ts
+++ src/validator/index.ts
@@ -1,9 +1,7 @@
+import { isHexStrict } from "web3-validator";
 import { UserOperation } from "../type";
 
 export type ValidInputTypes = Uint8Array | bigint | string | number | boolean;
-
-export const isHexStrict = (hex: ValidInputTypes): boolean =>
-  typeof hex === "string" && /^((-)?0x[0-9a-f]+|(0x))$/i.test(hex);
 
 /**
  * UserOperation a full user-operation struct. All fields MUST be set as hex values. empty bytes block (e.g. empty initCode) MUST be set to "0x"
